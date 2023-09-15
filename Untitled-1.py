7ff1c2b641dce92bbab6713a60dcd87be8a24bfe
SHarrison00 <86479780+SHarrison00@users.noreply.github.com>
2023-09-15 19:44:25 +0100

    landing page changes

--- Untitled-1.py
+++ Untitled-1.py
@@ -14,14 +14,11 @@ while not game.is_finished:
     
     game.board.display()
     game.get_player_move()
-    
     game.make_move()
 
     game.change_turn()
     game.update_valid_moves()
     game.update_scores()
-    print(game.black_score)
-    print(game.white_score)
     game.check_finished()
 
     time.sleep(0.1)
